53299e449f5ca2f0c2828de2194f2e9c13b56419
Rafal Bedkowski <bekowe.fara@gmail.com>
2022-08-17 10:03:49 +0200

    cleaning and seting up webpack kit to work

--- .history/webpack.common_20220816125638.js
+++ .history/webpack.common_20220816125638.js
@@ -5,6 +5,7 @@ module.exports = {
   /* here you can define another js file */
   entry: {
     index: "./src/js/index.js",
+    another: "./src/js/another.js",
   },
   output: {
     filename: "[name].[hash:8].js",
@@ -80,6 +81,12 @@ module.exports = {
       inject: true,
       chunks: ["index"],
       filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/another.html",
+      inject: true,
+      chunks: ["index", "another"],
+      filename: "another.html",
     }),
   ],
 };
